4339d57e01834f2c82789e90943ed007fe859a12
denis2797 <70007812+denis2797@users.noreply.github.com>
2025-03-02 14:11:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const channelId = '';
 
 async function login() {
   try {
-    await client.login(process.env.TOKEN);
+    await client.login(process.env.MTM0NTcxNzgyMzIwODY4OTY2NA.GUnWAi.2t527L-FLBPrtfa-RS8isKIBQKoTzVa-K0nUT0);
     console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
   } catch (error) {
     console.error('Failed to log in:', error);
